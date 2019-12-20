54b1dacdb7591a88afc94201c2dd6259aecb062d
Joey Pelletier <joeyp.pellerin@gmail.com>
2019-12-19 22:28:55 -0500

    Routing configuration + home component creation

---
+++ src/app/components/Home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { EtapeUnService } from './../../../services/2019/etapeUn.service';
+
+@Component({
+  selector: 'app-home-component',
+  templateUrl: 'home.component.html'
+})
+
+export class HomeComponent implements OnInit {
+  public reponse: string [];
+
+  constructor(private readonly etapeUnService: EtapeUnService) {
+    this.reponse = [];
+  }
+
+  ngOnInit() { }
+}
---
+++ src/app/components/component.module.ts
@@ -0,0 +1,10 @@
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [HomeComponent],
+  providers: [],
+})
+export class ComponentModule { }
